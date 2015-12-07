20d64288dd41708bb6d2a29bbff6afef705401f8
lydzmoraga <mojmoraga@gmail.com>
2015-12-07 22:05:40 +0800

    Checkpoint 23 added window.onload

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
 var animatePoints = function() {
  
-     var points = document.getElementsByClassName('point');
-    
-     var revealPoint = function(index) {
+    var pointsArray = document.getElementsByClassName('point');
+ 
+    var animatePoints = function(points) {
          points[index].style.opacity = 1;
          points[index].style.transform = "scaleX(1) translateY(0)";
          points[index].style.msTransform = "scaleX(1) translateY(0)";
@@ -14,3 +14,19 @@ var animatePoints = function() {
      }
  
  };
+
+ window.onload = function() {
+     
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         if (document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
