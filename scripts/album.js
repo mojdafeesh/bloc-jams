1f1efd3641860d3c70c39ea6d11a14d899c9afbe
lydzmoraga <mojmoraga@gmail.com>
2016-02-03 22:20:26 +0800

    revised album.js

--- scripts/album.js
+++ scripts/album.js
@@ -46,51 +46,58 @@
  };
 
 var createSongRow = function(songNumber, songName, songLength) {
-     var template =
-        '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>'
-      ;
- 
-     return template;
- };
 
- // #1
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+   var template =
+      '<tr class="album-view-song-item">'
+    + '  <td class="song-item-number">' + songNumber + '</td>'
+    + '  <td class="song-item-title">' + songName + '</td>'
+    + '  <td class="song-item-duration">' + songLength + '</td>'
+    + '</tr>'
+    ;
+
+   return template;
+
+};
 
 var setCurrentAlbum = function(album) {
-     // #2
-     albumTitle.firstChild.nodeValue = album.name;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
- 
-     // #3
-     albumSongList.innerHTML = '';
- 
-     // #4
-     for (i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
-     }
- };
- 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
- 
-    var albums = [albumPicasso, albumMarconi, albumAdele];
-    var index = 1;
-    albumImage.addEventListener("click", function(event)) {
-        setCurrentAlbum(albums[index]);
-        index++;
-        if (index == albums.length) {
-            index = 0;
-        }
-    });
- };
 
+  var albumTitle = document.getElementsByClassName('album-view-title')[0];
+  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+  var albumImage = document.getElementsByClassName('album-cover-art')[0];
+  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+   albumTitle.firstChild.nodeValue = album.name;
+   albumArtist.firstChild.nodeValue = album.artist;
+   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+   albumImage.setAttribute('src', album.albumArtUrl);
+
+   albumSongList.innerHTML = '';
+
+   for (i = 0; i < album.songs.length; i++) {
+       albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+   }
+
+};
+
+window.onload = function() {
+
+  setCurrentAlbum(albumPicasso);
+
+  var count = 0;
+  var albumImage = document.getElementsByClassName('album-cover-art')[0];
+  var albums = [albumMarconi, albumPicasso, albumAdele];
+
+  albumImage.addEventListener("click", function( event ) {
+
+    setCurrentAlbum(albums[count]);
+
+    count++;
+
+    if( count === albums.length ){
+      count = 0;
+    }
+
+  }, false);
+
+};
